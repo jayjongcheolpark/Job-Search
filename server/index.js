8fc2bb9c6d1ecb833e7a379701b3fc241c581413
Jay Park <jay.jongcheol.park@gmail.com>
2017-12-21 01:28:20 -0500

    Setup Postgresql

--- server/index.js
+++ server/index.js
@@ -1,11 +1,44 @@
 const express = require('express')
-const utils = require('./utils/parseURL')
+const Sequelize = require('sequelize')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+
+const parseUtils = require('./utils/parseURL')
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const Op = Sequelize.Op;
+const sequelize = new Sequelize('postgres', 'postgres', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: Op, // use Sequelize.Op
+})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err)
+  })
 
 const app = express()
-const bodyParser = require('body-parser')
+app.use(cookieParser())
+app.use(
+  session({
+    secret: 'boj hcraes',
+    store: new SequelizeStore({
+      db: sequelize
+    }),
+    resave: false,
+    saveUninitialized: false
+  })
+)
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-utils.initBrowser().then(page => {
+parseUtils.initBrowser().then(page => {
   require('./routes/parseURLRoutes')(app, page)
 })
 
@@ -23,4 +56,26 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT)
+const server = app.listen(PORT)
+
+// this function is called when you want the server to die gracefully
+// i.e. wait for existing connections
+var gracefulShutdown = function() {
+  console.log("Received kill signal, shutting down gracefully.");
+  server.close(async function() {
+    console.log("Closed out remaining connections.");
+    process.exit()
+  });
+
+   // if after
+   setTimeout(function() {
+       console.error("Could not close connections in time, forcefully shutting down");
+       process.exit()
+  }, 10*1000);
+}
+
+// listen for TERM signal .e.g. kill
+process.on ('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.g. Ctrl-C
+process.on ('SIGINT', gracefulShutdown);
--- server/routes/parseURLRoutes.js
+++ server/routes/parseURLRoutes.js
@@ -1,9 +1,10 @@
-const utils = require('../utils/parseURL')
+const parseUtils = require('../utils/parseURL')
 
 module.exports = (app, page) => {
   var ret = app.post('/api/job', async (req, res, next) => {
     try {
-      await utils.parseURL(req.body.URL, page)
+      const jobInfo = await parseUtils.parseURL(req.body.URL, page)
+      console.log(jobInfo)
       res.send({})
     } catch (err) {
       res.status(400).send('Bad Request')
